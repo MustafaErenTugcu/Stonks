af383bf5f68558f4e91752ed74486c159e392d70
Beyza Sungar <sungarbeyza@gmail.com>
2025-03-27 13:56:51 +0300

    frontend second commit

--- BorsaTakip/src/app/highlights/highlights.component.ts
+++ BorsaTakip/src/app/highlights/highlights.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {OneCikanlar} from "../interfaces/one-cikanlar";
 
 @Component({
   selector: 'app-highlights',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./highlights.component.css']
 })
 export class HighlightsComponent {
+  allCards = Array.from({ length: 40 }, (_, i) => ({
+    id: i + 1,
+    title: `Kart ${i + 1}`,
+    description: `Bu, Kart ${i + 1} için açıklamadır.`
+  }));
+
+  pageSize: 12;
+  currentPage = 0;
+  onPageChange(event: any) {
+    // Pagination logic
+    const startIndex = event.first;
+    const endIndex = startIndex + event.rows;
+
+  }
+  senetler: OneCikanlar[] = [
+    {
+      senetKodu: 'ASELS',
+      senetAdi: 'Aselsan',
+      hedefFiyatVerenKurumSayisi: 10,
+      sonKapanis: 35.5,
+      ortHedefFiyat: 40.2,
+      ortGetiriPotansiyeli: '%13.24',
+      enYuksekHedefFiyat: 45.0,
+      enYuksekFiyatinGetiriPotansiyeli: '%26.76'
+    },
+    {
+      senetKodu: 'THYAO',
+      senetAdi: 'Türk Hava Yolları',
+      hedefFiyatVerenKurumSayisi: 8,
+      sonKapanis: 65.0,
+      ortHedefFiyat: 72.3,
+      ortGetiriPotansiyeli: '%11.23',
+      enYuksekHedefFiyat: 80.0,
+      enYuksekFiyatinGetiriPotansiyeli: '%23.08'
+    }
+  ];
+
 
 }
--- BorsaTakip/src/app/radar/radar.component.ts
+++ BorsaTakip/src/app/radar/radar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {StockData} from "../interfaces/stock-data";
+
 
 @Component({
   selector: 'app-radar',
@@ -6,5 +8,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./radar.component.css']
 })
 export class RadarComponent {
+  stockData: StockData[] = [
+    {
+      senetKodu: 'KARSN',
+      tavsiyeTipi: 'AI',
+      hedefFiyat: 15.30,
+      getiriPotansiyeli: 45.02,
+      oneriTarihi: '21.03.2025',
+      kapanis: 10.55,
+      oncekiHedef:45,
+      oncekiOneriTarihi:"12.11.2001",
+      kurumAdi: 'Alnus Yatırım'
+    },
+    {
+      senetKodu: 'MGROS',
+      tavsiyeTipi: 'Endeks Üstü Get.',
+      hedefFiyat: 819.20,
+      getiriPotansiyeli: 66.50,
+      oneriTarihi: '20.03.2025',
+      kapanis: 492.00,
+      oncekiHedef:45,
+      oncekiOneriTarihi:"12.11.2001",
+      kurumAdi: 'Tera Yatırım'
+    },
+    {
+      senetKodu: 'MAVI',
+      tavsiyeTipi: 'Endeks Üstü Get.',
+      hedefFiyat: 118.00,
+      getiriPotansiyeli: 74.30,
+      oneriTarihi: '20.03.2025',
+      kapanis: 67.70,
+      oncekiHedef:45,
+      oncekiOneriTarihi:"12.11.2001",
+      kurumAdi: 'Oyak Yatırım'
+    },
+    {
+      senetKodu: 'MAVI',
+      tavsiyeTipi: 'AI',
+      hedefFiyat: 124.80,
+      getiriPotansiyeli: 84.34,
+      oneriTarihi: '20.03.2025',
+      kapanis: 67.70,
+      oncekiHedef:45,
+      oncekiOneriTarihi:"12.11.2001",
+      kurumAdi: 'Ahlatcı Yatırım'
+    },
+    {
+      senetKodu: 'MAVI',
+      tavsiyeTipi: 'AI',
+      hedefFiyat: 120.00,
+      getiriPotansiyeli: 77.25,
+      oneriTarihi: '20.03.2025',
+      kapanis: 67.70,
+      oncekiHedef:45,
+      oncekiOneriTarihi:"12.11.2001",
+      kurumAdi: 'Alnus Yatırım'
+    }
+  ];
+  allCards = Array.from({ length: 40 }, (_, i) => ({
+    id: i + 1,
+    title: `Kart ${i + 1}`,
+    description: `Bu, Kart ${i + 1} için açıklamadır.`
+  }));
 
+  pageSize: 12;
+  currentPage = 0;
+  onPageChange(event: any) {
+    // Pagination logic
+    const startIndex = event.first;
+    const endIndex = startIndex + event.rows;
+
+  }
 }
