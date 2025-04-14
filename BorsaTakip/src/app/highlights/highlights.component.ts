411d459b81ce6466f17e7172d4154db632dbd218
MustafaErenTugcu <merentugcu@gmail.com>
2025-04-14 12:00:48 +0300

    öne cıkarınlar updated

--- BorsaTakip/src/app/highlights/highlights.component.ts
+++ BorsaTakip/src/app/highlights/highlights.component.ts
@@ -4,36 +4,35 @@ import { FeaturedService } from '../services/highlights.service';
 @Component({
   selector: 'app-highlights',
   templateUrl: './highlights.component.html',
-  styleUrls: ['./highlights.component.css']
 })
 export class HighlightsComponent implements OnInit {
   featuredData: any[] = [];
   displayedData: any[] = [];
-  pageSize: number = 5;
-  first: number = 0;
+  first = 0;
+  pageSize = 5;
 
   constructor(private featuredService: FeaturedService) {}
 
   ngOnInit(): void {
-    this.getFeaturedData();
-  }
-
-  getFeaturedData(): void {
     this.featuredService.getFeatured().subscribe({
       next: (data) => {
         this.featuredData = data;
-        this.displayedData = this.featuredData.slice(0, this.pageSize);
+        this.updateDisplayedData();
       },
       error: (err) => {
-        console.error('Öne çıkan hisseler alınamadı:', err);
-      }
+        console.error('Veri alınamadı:', err);
+      },
     });
   }
 
-  onPageChange(event: any): void {
+  updateDisplayedData() {
+    const startIndex = this.first;
+    const endIndex = this.first + this.pageSize;
+    this.displayedData = this.featuredData.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: any) {
     this.first = event.first;
-    const start = event.first;
-    const end = event.first + event.rows;
-    this.displayedData = this.featuredData.slice(start, end);
+    this.updateDisplayedData();
   }
 }
